156eab9634080a24ce99851fcf9a1949867d465b
Mostafa Srour <mostafa.sroor.1983@gmail.com>
2021-10-01 16:08:12 +0300

    Initial commit

---
+++ src/main/java/com/reactive/spring/boot/ReactiveSpringBootCourseApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringBootCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveSpringBootCourseApplication.class, args);
+    }
+
+}
